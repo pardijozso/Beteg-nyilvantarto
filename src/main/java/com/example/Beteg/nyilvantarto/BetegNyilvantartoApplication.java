c43fa733423b9c07b7a34b0f7b6645d6c73bc235
pardijozso <pardijozso@gmail.com>
2026-06-11 13:31:16 +0200

    Initial commit

---
+++ src/main/java/com/example/Beteg/nyilvantarto/BetegNyilvantartoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Beteg.nyilvantarto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetegNyilvantartoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetegNyilvantartoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Beteg/nyilvantarto/BetegNyilvantartoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Beteg.nyilvantarto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetegNyilvantartoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
