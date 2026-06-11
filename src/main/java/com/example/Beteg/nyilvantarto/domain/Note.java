d335edb1bef21915a7025ee68191222558a64e14
Molnár Péter <ranlom0503@gmail.com>
2026-06-11 15:25:36 +0200

    @Data @Builder @AllArgsConstructor @NoArgsConstructor added Note.java and Patient.java classes

--- src/main/java/com/example/Beteg/nyilvantarto/domain/Note.java
+++ src/main/java/com/example/Beteg/nyilvantarto/domain/Note.java
@@ -1,9 +1,17 @@
 package com.example.Beteg.nyilvantarto.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "notes")
 public class Note {
--- src/main/java/com/example/Beteg/nyilvantarto/domain/Patient.java
+++ src/main/java/com/example/Beteg/nyilvantarto/domain/Patient.java
@@ -1,11 +1,19 @@
 package com.example.Beteg.nyilvantarto.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "patients")
 public class Patient {
