91d445eefd9d00a972192e8748a345c95afe2210
pardijozso <pardijozso@gmail.com>
2026-06-11 14:16:36 +0200

    még egyszer

--- src/main/java/com/example/Beteg/nyilvantarto/BetegNyilvantartoApplication.java
+++ src/main/java/com/example/Beteg/nyilvantarto/BetegNyilvantartoApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BetegNyilvantartoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BetegNyilvantartoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BetegNyilvantartoApplication.class, args); }
 }
