23f0e8c445256dc192180fc570189bad7085d4eb
pardijozso <pardijozso@gmail.com>
2026-06-12 21:22:49 +0200

    javított yml

--- src/main/java/com/example/Beteg/nyilvantarto/domain/Patient.java
+++ src/main/java/com/example/Beteg/nyilvantarto/domain/Patient.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class Patient {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
