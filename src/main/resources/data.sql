f35acfd74a0aa9feeacad1cf95be29c359a21168
Molnár Péter <ranlom0503@gmail.com>
2026-06-15 20:27:50 +0200

    doctor-list.html, DoctorController working.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,17 @@
+INSERT INTO doctor (name)
+VALUES ('Dr. Kovács Anna');
+
+INSERT INTO doctor (name)
+VALUES ('Dr. Nagy Péter');
+
 INSERT INTO patients
-( name, birth_place, birth_date, mother_name, address, diagnosis)
+(name, birth_place, birth_date, mother_name, address, diagnosis)
 VALUES
     (
-     'Molnár Péter',
-     'Miskolc',
-     DATE '2000-01-01',
-     'Mrúz Tímea',
-     '3900 Szerencs, fő út 1',
-     'nátha'
-     );
+        'Molnár Péter',
+        'Miskolc',
+        DATE '2000-01-01',
+        'Mrúz Tímea',
+        '3900 Szerencs, fő út 1',
+        'nátha'
+    );
